a84f442abc55cc2a89b3d8c2117237b0c48c3462
alexeevya <yrasik1986@bk.ru>
2020-12-12 20:26:49 +0300

    add Avatar

---
+++ WebAlexeev90321.DAL/Migrations/20201212163248_Avatar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAlexeev90321.DAL.Migrations
+{
+    public partial class Avatar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "AvatarImage",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarImage",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ WebAlexeev90321/Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAlexeev90321.DAL.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAlexeev90321.Controllers
+{
+    public class ImageController : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+        IWebHostEnvironment _env;
+        public ImageController(UserManager<ApplicationUser>
+       userManager, IWebHostEnvironment env)
+        {
+            _userManager = userManager;
+            _env = env;
+        }
+        public async Task<FileResult> GetAvatar()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user.AvatarImage != null)
+                return File(user.AvatarImage, "image/...");
+            else
+            {
+                var avatarPath = "/Images/anonymous.png";
+
+                return File(_env.WebRootFileProvider
+                .GetFileInfo(avatarPath)
+               .CreateReadStream(), "image/...");
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
