c8db5d3cd43117c4256235572d6c4fd9145137ea
alexeevya <yrasik1986@bk.ru>
2020-12-14 17:19:19 +0300

    LR_10 second commit

--- WebAlexeev90321.Blazor/Startup.cs
+++ WebAlexeev90321.Blazor/Startup.cs
@@ -29,6 +29,7 @@ namespace WebAlexeev90321.Blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +48,7 @@ namespace WebAlexeev90321.Blazor
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            
 
             app.UseRouting();
 
