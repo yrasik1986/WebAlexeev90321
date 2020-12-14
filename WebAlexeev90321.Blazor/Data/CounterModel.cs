8187ec13cb8eda37141efafc0c40969d326a439d
alexeevya <yrasik1986@bk.ru>
2020-12-14 16:53:25 +0300

    LR_10

---
+++ WebAlexeev90321.Blazor/Data/CounterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAlexeev90321.Blazor.Data
+{
+    public class CounterModel
+    {
+        [DataType("int")]
+        [Range(0, int.MaxValue)]
+        public int CounterValue { get; set; }
+    }
+}
---
+++ WebAlexeev90321.Blazor/Data/RadioComponentListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+
+namespace WebAlexeev90321.Blazor.Data
+{
+    public class RadioComponentListViewModel
+    {
+        [JsonPropertyName("radioComponentId")]
+        public int RadioComponentId { get; set; } // id 
+        [JsonPropertyName("radioComponentName")]
+        public string RadioComponentName { get; set; } // название блюда
+    }
+
+    public class RadioComponentDetailsViewModel
+    {
+        [JsonPropertyName("radioComponentName")]
+        public string RadioComponentName { get; set; } // название 
+        [JsonPropertyName("description")]
+        public string Description { get; set; } // описание блюда
+        [JsonPropertyName("price")]
+        public int Price { get; set; } // цена
+        [JsonPropertyName("image")]
+        public string Image { get; set; } // имя файла изображения
+    }
+
+}
